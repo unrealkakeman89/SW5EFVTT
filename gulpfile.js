0906bab02e7663c645b558761a35534c53ff34b0
Jacob Lucas <jtljac2@gmail.com>
2021-06-24 13:11:46 +0100

    Moved most files for new system

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,71 @@ const gulp = require("gulp");
 const less = require("gulp-less");
 
 /* ----------------------------------------- */
+/*  Constants
+/* ----------------------------------------- */
+
+const src = "./src/"
+const dest = "./dist/"
+
+/* ----------------------------------------- */
 /*  Compile LESS
 /* ----------------------------------------- */
 
-const SW5E_LESS = ["less/**/*.less"];
+const SW5E_LESS = [src + "less/**/*.less"];
 
 function compileLESS() {
-  return gulp.src("less/original/sw5e.less").pipe(less()).pipe(gulp.dest("./"));
+  return gulp.src(src + "less/original/sw5e.less")
+      .pipe(less())
+      .pipe(gulp.dest(dest));
 }
 
 function compileGlobalLess() {
-  return gulp.src("less/update/sw5e-global.less").pipe(less()).pipe(gulp.dest("./"));
+  return gulp.src(src + "less/update/sw5e-global.less")
+      .pipe(less())
+      .pipe(gulp.dest(dest));
 }
 
 function compileLightLess() {
-  return gulp.src("less/update/sw5e-light.less").pipe(less()).pipe(gulp.dest("./"));
+  return gulp.src(src + "less/update/sw5e-light.less")
+      .pipe(less())
+      .pipe(gulp.dest(dest));
 }
 
 function compileDarkLess() {
-  return gulp.src("less/update/sw5e-dark.less").pipe(less()).pipe(gulp.dest("./"));
+  return gulp.src(src + "less/update/sw5e-dark.less")
+      .pipe(less())
+      .pipe(gulp.dest(dest));
 }
 
-const css = gulp.series(compileLESS, compileGlobalLess, compileLightLess, compileDarkLess);
+const css = gulp.parallel(compileLESS, compileGlobalLess, compileLightLess, compileDarkLess);
+
+/* ----------------------------------------- */
+/*  Copy Files
+/* ----------------------------------------- */
+
+const SW5E_COPY = [src + "**", "!" + src + "less/**/*.less"]
+
+async function copy() {
+  return new Promise((resolve, reject) => {
+    gulp.src("README.md").pipe(gulp.dest(dest));
+    gulp.src(src + "system.json").pipe(gulp.dest(dest));
+    gulp.src(src + "template.json").pipe(gulp.dest(dest));
+    gulp.src(src + "sw5e.js").pipe(gulp.dest(dest));
+    gulp.src(src + "fonts/**").pipe(gulp.dest(dest + "fonts/"));
+    gulp.src(src + "lang/**").pipe(gulp.dest(dest + "lang/"));
+    gulp.src(src + "module/**").pipe(gulp.dest(dest + "module/"));
+    gulp.src(src + "packs/**").pipe(gulp.dest(dest + "packs/"));
+    gulp.src(src + "templates/**").pipe(gulp.dest(dest + "templates/"));
+    gulp.src(src + "ui/**").pipe(gulp.dest(dest + "ui/"));
+    resolve();
+  });
+}
+
+/* ----------------------------------------- */
+/*  build
+/* ----------------------------------------- */
+
+const build = gulp.parallel(css, copy)
 
 /* ----------------------------------------- */
 /*  Watch Updates
@@ -31,11 +74,12 @@ const css = gulp.series(compileLESS, compileGlobalLess, compileLightLess, compil
 
 function watchUpdates() {
   gulp.watch(SW5E_LESS, css);
+  gulp.watch(SW5E_COPY, copy);
 }
 
 /* ----------------------------------------- */
 /*  Export Tasks
 /* ----------------------------------------- */
 
-exports.default = css;
-gulp.parallel(css), (exports.watch = gulp.series(gulp.parallel(css), watchUpdates));
+exports.default = build;
+exports.watch = watchUpdates;
